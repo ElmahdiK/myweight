b65e8ded255983b18b921182890b6d68cf0b0d73
ElmahdiK <elmahdi.korfed@gmail.com>
2025-04-12 08:43:34 +0200

    feat: enhance data visualization and layout improvements

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,37 +8,73 @@
 window.onload = () => {
     let arrX = ["x"];
     let arrY = ["weight (kg)"];
+
     const dataSet = [
-        [Date.UTC(2016, 6, 24), 83.9], //2016
-        // [Date.UTC(2016, 9, 24), 90],
-        // [Date.UTC(2016, 11, 6), 87.5],
-        // [Date.UTC(2017, 0, 4), 87.1], //2017
-        // [Date.UTC(2017, 1, 27), 87.9],
+        [Date.UTC(2016, 6, 24), 83.9],
+        [Date.UTC(2016, 9, 24), 90],
+        [Date.UTC(2016, 11, 6), 87.5],
+        [Date.UTC(2017, 0, 4), 87.1],
+        [Date.UTC(2017, 1, 27), 87.9],
         [Date.UTC(2017, 5, 17), 79.8],
-        // [Date.UTC(2017, 7, 26), 85.0],
-        // [Date.UTC(2017, 11, 24), 83],
-        // [Date.UTC(2018, 0, 1), 84.2], //2018
-        // [Date.UTC(2018, 0, 8), 82],
-        // [Date.UTC(2018, 3, 1), 83.5],
-        // [Date.UTC(2018, 10, 1), 85.1],
-        [Date.UTC(2019, 1, 10), 78.7], //2019
-        // [Date.UTC(2019, 6, 29), 84.9],
-        // [Date.UTC(2019, 10, 30), 86.2],
-        // [Date.UTC(2019, 11, 28), 85.9],
-        // [Date.UTC(2020, 1, 18), 89.4], //2020
-        // [Date.UTC(2020, 5, 29), 89.7],
-        // [Date.UTC(2020, 6, 25), 92.5],
+        [Date.UTC(2017, 7, 26), 85.0],
+        [Date.UTC(2017, 11, 24), 83],
+        [Date.UTC(2018, 0, 1), 84.2],
+        [Date.UTC(2018, 0, 8), 82],
+        [Date.UTC(2018, 3, 1), 83.5],
+        [Date.UTC(2018, 10, 1), 85.1],
+        [Date.UTC(2019, 1, 10), 78.7],
+        [Date.UTC(2019, 6, 29), 84.9],
+        [Date.UTC(2019, 10, 30), 86.2],
+        [Date.UTC(2019, 11, 28), 85.9],
+        [Date.UTC(2020, 1, 18), 89.4],
+        [Date.UTC(2020, 5, 29), 89.7],
+        [Date.UTC(2020, 6, 25), 92.5],
         [Date.UTC(2020, 9, 24), 93.6],
-        // [Date.UTC(2020, 10, 20), 90.0],
-        // [Date.UTC(2021, 5, 13), 88.4], //2021
-        [Date.UTC(2022, 4, 2), 99.8], //2022
-        // [Date.UTC(2022, 5, 11), 104.3],
-        // [Date.UTC(2024, 1, 19), 107.2], //2024
-        [Date.UTC(2024, 2, 26), 106]
+        [Date.UTC(2020, 10, 20), 90.0],
+        [Date.UTC(2021, 5, 13), 88.4],
+        [Date.UTC(2022, 4, 2), 99.8],
+        [Date.UTC(2022, 5, 11), 104.3],
+        [Date.UTC(2023, 5, 3), 104.1],
+        [Date.UTC(2023, 10, 4), 105.2],
+        [Date.UTC(2024, 1, 19), 107.2],
+        [Date.UTC(2024, 2, 26), 106],
+        [Date.UTC(2024, 5, 1), 111.3],
+        [Date.UTC(2024, 9, 14), 108.5],
+        [Date.UTC(2025, 1, 14), 111.7],
+        [Date.UTC(2025, 3, 12), 105.5]
     ];
 
-    // dataTables
-    dataSet.forEach(r => {
+    // ----- dataTables -----
+    new DataTable('#arrayWeight', {
+        columns: [
+            {
+                title: 'Weight (kg)',
+                data: 1
+            },
+            {
+                title: 'Date',
+                data: 0,
+                render: function (data, type, row) {
+                    const timestamp = typeof data === 'number' ? data : Date.parse(data);
+                    if (isNaN(timestamp)) return 'Invalid Date';
+
+                    if (type === 'display' || type === 'filter') return formatDate(timestamp);
+                    return timestamp;
+                }
+            },
+        ],
+        columnDefs: [
+            {
+                targets: 0, // Colonne "Weight"
+                className: 'text-left' // Aligner à gauche
+            }
+        ],
+        data: dataSet
+    });
+
+    // ----- billboard -----
+    const copyDataSet = JSON.parse(JSON.stringify(dataSet));
+    copyDataSet.forEach(r => {
         arrX.push(r[0]);
         arrY.push(r[1]);
 
@@ -50,16 +86,6 @@ window.onload = () => {
         div2.innerHTML = r[1];
         r[1] = div2;
     })
-
-    new DataTable('#arrayWeight', {
-        columns: [
-            { title: 'Date' },
-            { title: 'Weight (kg)' },
-        ],
-        data: dataSet
-    });
-
-    // billboard
     bb.generate({
         data: {
             x: "x",
@@ -81,3 +107,11 @@ window.onload = () => {
         bindto: "#graphWeight"
     });
 };
+
+const formatDate = (timestamp) => {
+    const d = new Date(timestamp);
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    return `${day}/${month}/${year}`;
+}
